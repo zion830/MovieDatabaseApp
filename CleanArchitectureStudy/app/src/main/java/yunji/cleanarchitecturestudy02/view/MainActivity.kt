429d9c22962e7f5d1c724dcfc662629e222d26e6
zion830 <zion830@honglab.org>
2020-03-30 21:39:41 +0900

    feat: ActionBar에 SearchView 추가

--- CleanArchitectureStudy/app/src/main/java/yunji/cleanarchitecturestudy02/view/MainActivity.kt
+++ CleanArchitectureStudy/app/src/main/java/yunji/cleanarchitecturestudy02/view/MainActivity.kt
@@ -1,6 +1,12 @@
 package yunji.cleanarchitecturestudy02.view
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import yunji.cleanarchitecturestudy02.R
@@ -15,6 +21,7 @@ import yunji.cleanarchitecturestudy02.util.showToast
 import yunji.cleanarchitecturestudy02.viewmodel.MainViewModel
 import yunji.cleanarchitecturestudy02.viewmodel.MainViewModelFactory
 
+
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
     private val movieRecyclerAdapter = MoviePagedRecyclerAdapter()
 
@@ -28,15 +35,14 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
         initAdapter()
         initView()
         observeUiData()
+        handleIntent(intent)
     }
 
     private fun initAdapter() {
         movieRecyclerAdapter.apply {
             onItemClickListener = OnSingleClickListener.wrap(
                 object : OnItemClickListener<Movie> {
-                    override fun onClick(item: Movie) {
-                        showToast(item.toString())
-                    }
+                    override fun onClick(item: Movie) = showToast(item.toString())
                 })
         }
     }
@@ -52,6 +58,36 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
     private fun observeUiData() {
         with(viewModel) {
             msgText.observe(this@MainActivity, Observer { showToast(it) })
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.menu_search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.menu_sort -> {
+            true
+        }
+        else -> false
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            //use the query to search your data somehow
         }
     }
 }
